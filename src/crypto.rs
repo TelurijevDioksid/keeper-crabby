44c30d49ee518a739345750dd7ff225a53a4fc23
Teo Radolovict <tr54427@fer.hr>
2024-07-10 23:13:08 +0200

    Added aes128

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,4 +1,14 @@
 use sha2::{Sha256, Digest};
+use aes_gcm_siv::{
+    aead::{self, consts::U12, generic_array::GenericArray, Aead, KeyInit, OsRng},
+    Aes128GcmSiv, Key, Nonce,
+};
+
+pub struct CipherConfig {
+    pub key: Key<Aes128GcmSiv>,
+    pub nonce: GenericArray<u8, U12>,
+    pub ciphertext: Vec<u8>,
+}
 
 pub fn hash(data: String) -> String {
     let mut hasher = Sha256::new();
@@ -6,3 +16,20 @@ pub fn hash(data: String) -> String {
     let result = hasher.finalize();
     format!("{:x}", result)
 }
+
+pub fn encrypt_data(data: &str) -> Result<CipherConfig, aead::Error> {
+    let key = Aes128GcmSiv::generate_key(&mut OsRng);
+    let cipher = Aes128GcmSiv::new(&key);
+    let nonce = Nonce::from_slice(b"unique nonce");
+    let ciphertext = cipher.encrypt(nonce, data.as_bytes())?;
+    return Ok(CipherConfig { key, nonce: *nonce, ciphertext });
+}
+
+pub fn decrypt_data(config: &CipherConfig) -> Result<String, aead::Error> {
+    let cipher = Aes128GcmSiv::new(&config.key);
+    let plaintext = cipher.decrypt(&config.nonce, config.ciphertext.as_ref())?;
+    let result = String::from_utf8(plaintext).unwrap();
+    return Ok(result);
+}
+
+
